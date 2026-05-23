b5b47bfb8f37979ff069ced0b300bfc222fc1529
Camilo Torres <fctorres@uts.edu.co>
2026-05-23 16:42:41 -0500

    feat: implementar HU-04 estructura base de plantilla con arquitectura de capas

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI(title="API de Gestión de Hojas de Vida")
-
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenido a la API de Gestión de Hojas de Vida", "status": "Ready"}
